078dc1f71c05dac608bd8c3770138a3fa6b78e40
Kjetil Orbekk <kjetil.orbekk@gmail.com>
2015-06-09 21:56:36 -0400

    Add vim and i3 configs.

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 red='\e[0;31m'
+orange='\e[0;33m'
 green='\e[0;32m'
 none='\e[0m'
 
@@ -15,15 +16,52 @@ fi
 # Does nothing and prints an error message if $2 exists and is not a symlink.
 create_symlink() {
   if [[ -e "$2" && ! -h "$2" ]]; then
-    echo -e "${red}[WARNING]${none} '$2' exists and is not a symlink. Skipped."
+    echo -e "${orange}[SKIPPED]${none} '$2' exists and is not a symlink."
+    return
   else
     if ln -sf "$1" "$2"; then
       echo -e "${green}[OK]${none} '$2' → '$1'"
     else
-      echo -e "${red}[WARNING] Could not create '$2'"
+      echo -e "${red}[WARNING]${none} could not create '$2'"
     fi
+  fi
+  if ! diff "$2" "$(dirname $2)/$1"; then
+    echo -e "${red}[WARNING]${none} diffs in $2"
   fi
 }
 
 create_symlink dotfiles/gitconfig .gitconfig
-create_symlink dotfiles/zshrc .zshrc
+create_symlink /dev/null .vimrc.local
+
+if [[ "${SHELL}" = *zsh* ]]; then
+  create_symlink dotfiles/zshrc .zshrc
+else
+  echo -e "${orange}[SKIPPED]${none} shell is not zsh :-(."
+fi
+
+if which i3 >/dev/null; then
+  mkdir -p .i3
+  create_symlink ../dotfiles/i3/config .i3/config
+else
+  echo -e "${orange}[SKIPPED]${none} i3 not installed."
+fi
+
+create_symlink dotfiles/vimrc .vimrc
+if which git >/dev/null; then
+  if [[ -e .vim/bundle/Vundle.vim ]]; then
+    echo -e "${orange}[SKIPPED]${none} Vundle.vim already installed"
+  else
+    success=1
+    mkdir -p .vim/bundle
+    git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim \
+      || success=0
+    vim +PluginInstall +qall || success=0
+    if [[ "$success" == "1" ]]; then
+      echo -e "${green}[OK]${none} installed vim plugins"
+    else
+      echo -e "${red}[WARNING]${none} failed to install vim plugins"
+    fi
+  fi
+else
+  echo -e "${red}[WARNING]${none} git not installed"
+fi
---
+++ vimrc
@@ -0,0 +1,80 @@
+let mapleader = ","
+syntax on
+
+filetype off
+set rtp+=~/.vim/bundle/Vundle.vim
+call vundle#begin()
+" TODO(orbekk): Try ctrlp.vim instead.
+" Plugin 'wincent/command-t'
+Plugin 'chriskempson/vim-tomorrow-theme'
+Plugin 'farseer90718/vim-taskwarrior'
+Plugin 'gmarik/Vundle.vim'
+call vundle#end()
+
+set modeline
+set tabpagemax=20
+set autoread
+set noswapfile
+set timeoutlen=1000
+set ignorecase
+set expandtab
+set smarttab
+set smartcase
+set incsearch
+set autoindent
+set formatoptions=crt
+setglobal fileencoding=utf-8
+set fileencodings=ucs-bomb,utf-8,latin1
+set virtualedit=block
+set hidden
+set confirm
+inoremap <C-c> <nop>
+set hlsearch
+
+filetype plugin on
+filetype indent on
+
+setlocal complete+=ktags
+
+nmap <silent> <C-N> :silent noh<CR>
+map <leader>cd :cd %:p:h<CR>:pwd<CR>
+
+set wildignore+=*.class,target/*,project/*
+
+set guifont=DroidSansMono\ 10
+set guioptions-=m
+set guioptions-=T
+set guioptions-=r
+colorscheme Tomorrow
+
+set colorcolumn=81
+
+map <leader>R :source ~/.vimrc<CR>
+
+function! GetFileBase()
+  return substitute(expand("%"),
+      \ '\(.\{-}\)\(_test\|_unittest\)\?\.\(h\|cc\)$', '\1', "")
+endfunction
+
+" Switch between cc, h, test/unittest files.
+function! EditCc()
+  exec "edit " . fnameescape(GetFileBase() . ".cc")
+endfunction
+map <leader>cc :silent :call EditCc()<CR>
+
+function! EditH()
+  exec "edit " . fnameescape(GetFileBase() . ".h")
+endfunction
+map <leader>h :silent :call EditH()<CR>
+
+function! EditTest()
+  let file_base = GetFileBase()
+  if (filereadable(file_base . "_unittest.cc"))
+    exec "edit " . fnameescape(file_base . "_unittest.cc")
+  else
+    exec "edit " . fnameescape(file_base . "_test.cc")
+  endif
+endfunction
+map <leader>te :silent :call EditTest()<CR>
+
+source ~/.vimrc.local
