b48ff88a8b6ee94fd70e88127800fdbe29858805
Kjetil Orbekk <kjetil.orbekk@gmail.com>
2015-10-11 10:57:22 -0400

    Tweak brightness increments.

--- bin/brightness.sh
+++ bin/brightness.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 readonly XBACKLIGHT=xbacklight
-readonly levels=(0 1 2 3 4 6 10 15 20 25 30 40 50 60 70 80 90 100)
 
 current_level=$($XBACKLIGHT)
 current_level=$((${current_level%.*}))
@@ -9,10 +8,8 @@ if (( "$current_level" < 8 )); then
     diff=1
 elif (( "$current_level" < 24 )); then
     diff=6
-elif (( "$current_level" < 50 )); then
-    diff=12
 else
-    diff=24
+    diff=10
 fi
 
 if [[ $1 == "+" ]]; then
