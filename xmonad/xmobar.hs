d9ff5f08b081461a45343a5fcd06688e082c3423
Kjetil Orbekk <kjetil.orbekk@gmail.com>
2016-07-05 09:25:23 -0400

    Xmobar customization.

--- xmonad/xmobar.hs
+++ xmonad/xmobar.hs
@@ -6,45 +6,47 @@ Config { font = "xft:Source Code Pro:size=8:bold:antialias=true" -- "-*-Fixed-Bo
        , fgColor = "grey"
        , position = TopP 0 100 -- TopW L 95
        , commands = [
-                    --  Run Weather "EGPF" ["-t","<skyCondition> <tempC>°C","-L","64","-H","77","--normal","green","--high","red","--low","lightblue"] 36000
-                      Run Weather "KNYC"
-                              ["-t", "NYC: <skyCondition> <tempC>°C <rh>%"
-                              , "-L", "15", "-H", "25", "--normal", "green"
-                              , "--high", "red", "--low", "lightblue"] 36000
-                    , Run Cpu ["-L","3","-H","50","--normal","green","--high","red"] 10
-                    , Run Memory ["-t","Mem: <usedratio>%"] 10
-                    , Run Swap [] 10
-                    , Run Date "%a %b %_d %l:%M" "date" 10
-                    , Run DiskU [("/", "/: <used>/<size>")]
-                          ["-L", "20", "-H", "50", "-m", "1", "-p", "3"] 20
-                    , Run StdinReader
-                    , Run DynNetwork     [
-                           "--template" , "<dev>: <tx>kB/s|<rx>kB/s"
-                          , "--Low"      , "1000"       -- units: kB/s
-                          , "--High"     , "5000"       -- units: kB/s
-                          , "--low"      , "green"
-                          , "--normal"   , "orange"
-                          , "--high"     , "red"
-                          ] 10
-                    , Run BatteryP ["BAT0"] [
-                           "--template" , "Batt: <acstatus>"
-                          , "--Low"      , "10"        -- units: %
-                          , "--High"     , "80"        -- units: %
-                          , "--low"      , "darkred"
-                          , "--normal"   , "darkorange"
-                          , "--high"     , "darkgreen"
-                          , "--" -- battery specific options
-                                 -- discharging status
-                          , "-o"	, "<left>% (<timeleft>)"
-                          -- AC "on" status
-                          , "-O"	, "<fc=#ee9a00>Charging</fc>"
-                          -- charged status
-                          , "-i"	, "<fc=#00ff00>Charged</fc>"
-                          ] 50
-                    ]
+           --  Run Weather "EGPF" ["-t","<skyCondition> <tempC>°C","-L","64","-H","77","--normal","green","--high","red","--low","lightblue"] 36000
+           Run Weather "KNYC"
+                     ["-t", "NYC: <skyCondition> <tempC>°C <rh>%"
+                     , "-L", "15", "-H", "25", "--normal", "green"
+                     , "--high", "red", "--low", "lightblue"] 36000
+         , Run Cpu ["-L","3","-H","50","--normal","green","--high","red"] 10
+         , Run Volume "default" "Master" [] 10
+         , Run Brightness ["-t", "<fc=#ffffff><percent>%</fc>"] 10
+         , Run Memory ["-t","Mem: <usedratio>%"] 10
+         -- , Run Swap [] 10
+         , Run Date "%a %b %_d %l:%M" "date" 10
+         , Run DiskU [("/", "/: <used>/<size>")]
+               ["-L", "20", "-H", "50", "-m", "1", "-p", "3"] 1000
+         , Run StdinReader
+         , Run DynNetwork     [
+                "--template" , "<dev>: <tx>kB/s|<rx>kB/s"
+               , "--Low"      , "1000"       -- units: kB/s
+               , "--High"     , "5000"       -- units: kB/s
+               , "--low"      , "green"
+               , "--normal"   , "orange"
+               , "--high"     , "red"
+               ] 100
+         , Run BatteryP ["BAT0"] [
+                "--template" , "Batt: <acstatus>"
+               , "--Low"      , "10"        -- units: %
+               , "--High"     , "80"        -- units: %
+               , "--low"      , "darkred"
+               , "--normal"   , "darkorange"
+               , "--high"     , "darkgreen"
+               , "--" -- battery specific options
+                      -- discharging status
+               , "-o"	, "<left>% (<timeleft>)"
+               -- AC "on" status
+               , "-O"	, "<fc=#ee9a00>Charging</fc>"
+               -- charged status
+               , "-i"	, "<fc=#00ff00>Charged</fc>"
+               ] 50
+         ]
        , sepChar = "%"
        , alignSep = "}{"
-       , template = "%StdinReader% }{ %battery% | %dynnetwork% | %disku% | %cpu% | %memory% * %swap%    <fc=#ee9a00>%date%</fc> | %KNYC%"
+       , template = "%StdinReader% }{ %default:Master% %bright% %battery% | %dynnetwork% | %disku% | %cpu% | %memory% <fc=lightblue>%date%</fc> | %KNYC%"
        }
 
 -- Config {
