8aed9b56a1211e0df4b44d93b3f5fdc7766cba0c
Kjetil Orbekk <kjetil.orbekk@gmail.com>
2020-01-19 09:02:54 -0500

    synergy

--- nix-home-config/aji.nix
+++ nix-home-config/aji.nix
@@ -3,7 +3,8 @@
 {
   imports = [
     ./my-env.nix
-    # ./synergy-server.nix
+    ./synergy-server.nix
+    # ./synergy-client.nix
   ];
 
   home.stateVersion = "19.03";
--- nix-home-config/pincer.nix
+++ nix-home-config/pincer.nix
@@ -3,7 +3,7 @@
 {
   imports = [
     ./my-env.nix
-    ./synergy-server.nix
+    ./synergy-client.nix
   ];
 
   home.stateVersion = "19.03";
