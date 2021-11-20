17253bd03d46423b7e453db9a6eb92ac5fe6733f
Kjetil Orbekk <kj@orbekk.com>
2021-11-20 06:38:42 -0500

    emacs: enable emoji

--- emacs/.doom.d/init.el
+++ emacs/.doom.d/init.el
@@ -49,6 +49,7 @@
        window-select     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
        ;;zen               ; distraction-free coding or writing
+       (emoji +unicode)  ; 😇
 
        :editor
        (evil +everywhere); come to the dark side, we have cookies
